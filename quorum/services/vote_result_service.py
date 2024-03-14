a35b866f68f9412e0d78d0bc82d92852908ac42b
leonardo.perrella <leonardo.perrella@yahoo.com.br>
2024-03-14 20:15:41 -0300

    feat: function vote result context

---
+++ quorum/services/vote_result_service.py
@@ -0,0 +1,59 @@
+from typing import Dict, List
+import pandas as pd
+
+
+def vote_result_context():
+    df_vote_result = load_vote_result()
+    df_legislators = load_legislators()
+
+    df_grouped_vote_result = df_vote_result.groupby(
+        ['legislator_id', 'vote_type'], as_index=False).count()
+
+    df_merged = pd.merge(df_grouped_vote_result, df_legislators, how='inner',
+                         left_on='legislator_id', right_on='id', validate=None)
+
+    result = mount_base_structure(df_merged)
+
+    resolve_votes(df_merged, result)
+
+    return result
+
+
+def load_vote_result() -> pd.DataFrame:
+    return csv_to_df('quorum/db/csv/vote_results.csv')
+
+
+def load_legislators() -> pd.DataFrame:
+    return csv_to_df('quorum/db/csv/legislators.csv')
+
+
+def csv_to_df(path: str) -> pd.DataFrame:
+    return pd.read_csv(path)
+
+
+def mount_base_structure(df: pd.DataFrame) -> List[Dict]:
+    df_legislator_unified = df[[
+        'legislator_id', 'name']].drop_duplicates()
+
+    base_structure = []
+    for _, row in df_legislator_unified.iterrows():
+        base_structure.append({
+
+            "id": row['legislator_id'],
+            "name": row['name'],
+            "suported_bills": 0,
+            "opposed_bills": 0
+
+        })
+
+    return base_structure
+
+
+def resolve_votes(df: pd.DataFrame, result: List) -> None:
+    for _, row in df.iterrows():
+        if d := next((item for item in result if item['id'] == row['legislator_id']), None):
+            if row['vote_type'] == 1:
+                d['suported_bills'] += row['id_x']
+
+            elif row['vote_type'] == 2:
+                d['opposed_bills'] += row['id_x']
---
+++ tests/unit/test_vote_results.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from quorum.services.vote_result_service import vote_result_context
+
+
+def test_vote_result_context(mocker):
+
+    volte_results_data = {
+        'id': [1, 2, 3, 4, 5],
+        'legislator_id': [1, 1, 1, 2, 2],
+        'vote_id': [1, 2, 1, 1, 2],
+        'vote_type': [1, 2, 2, 1, 2]
+    }
+
+    df_volte_results = pd.DataFrame(volte_results_data)
+
+    mocker.patch(
+        "quorum.services.vote_result_service.load_vote_result",
+        return_value=df_volte_results
+    )
+
+    legislators_data = {
+        'id': [1, 2],
+        'name': ['Lorem', "Ipsum"],
+
+    }
+
+    df_legislators_data = pd.DataFrame(legislators_data)
+
+    mocker.patch(
+        "quorum.services.vote_result_service.load_legislators",
+        return_value=df_legislators_data
+    )
+
+    assert vote_result_context()[0] == {
+        "id": 1,
+        "name": "Lorem",
+        "suported_bills": 1,
+        "opposed_bills": 2
+    }
