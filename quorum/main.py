0d49fcad5c804ce92c9e045fd955876078010c89
leonardo.perrella <leonardo.perrella@yahoo.com.br>
2024-03-14 22:08:56 -0300

    feat: add info index

--- quorum/main.py
+++ quorum/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from quorum.services.legislator_result_service import legislator_result_context
+from quorum.services.bill_result_service import bill_result_context
 
 app = FastAPI()
 
@@ -14,8 +15,11 @@ templates = Jinja2Templates(directory="quorum/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    vote_result = legislator_result_context()
+    legislator_result = legislator_result_context()
+    bill_result = bill_result_context()
     return templates.TemplateResponse(
         request=request,
         name="index.html",
-        context={"vote_result": vote_result})
+        context={
+            "legislator_result": legislator_result,
+            "bill_result": bill_result})
